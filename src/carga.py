c9cc5884da56a9bd6526c0c976c87ae5e90c765d
Victor Goveia <victor.goov@gmail.com>
2025-04-15 12:12:06 -0300

    added new function to create db if not exists

--- src/carga.py
+++ src/carga.py
@@ -1,6 +1,27 @@
 import pandas as pd
+import psycopg2
+
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from sqlalchemy import create_engine
 from src.config import DB_CONFIG
+
+
+def create_database_if_not_exists(dbname, user, password, host="localhost", port=5432):
+    conn = psycopg2.connect(
+        dbname="postgres", user=user, password=password, host=host, port=port
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{dbname}'")
+    exists = cur.fetchone()
+    if not exists:
+        cur.execute(f"CREATE DATABASE {dbname}")
+        print(f">>> Banco de dados '{dbname}' criado.")
+    else:
+        print(f">>> Banco '{dbname}' já existe.")
+    cur.close()
+    conn.close()
+
 
 def send_to_postgres(df, tabela="obitos_tratados"):
     conn_str = (
